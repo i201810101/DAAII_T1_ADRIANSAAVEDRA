8a34ea98cc00ce4f543f91e440261c17575659ff
i201810101 <i201810101@cibertec.edu.pe>
2024-06-07 18:11:28 -0500

    initial commit

---
+++ src/main/java/pe/edu/cibertec/DAAII_T1_ADRIANSAAVEDRA/DaaiiT1AdriansaavedraApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAAII_T1_ADRIANSAAVEDRA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaaiiT1AdriansaavedraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaaiiT1AdriansaavedraApplication.class, args);
+	}
+
+}
